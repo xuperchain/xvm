07fc08e0ef5fbff15402dac6e4687422e05e221e
chenfengjin <1871653365@qq.coom>
2021-05-21 11:27:23 +0800

    RFC: Add wasi resolver

--- runtime/wasi/resolver.go
+++ runtime/wasi/resolver.go
@@ -37,6 +37,43 @@ var resolver = exec.MapResolver(map[string]interface{}{
 		exec.Throw(exec.NewTrap("exit"))
 		return 0
 	},
+	"env.___wasi_fd_write": func(ctx exec.Context, x, y, z, w uint32) uint32 {
+		return 8
+	},
+	"env.___wasi_fd_prestat_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 8
+	},
+	"env.___wasi_fd_fdstat_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 8
+	},
+	"env.___wasi_fd_prestat_dir_name": func(ctx exec.Context, x, y, z uint32) uint32 {
+		return 8
+	},
+	"env.___wasi_fd_close": func(ctx exec.Context, x uint32) uint32 {
+		return 8
+	},
+	"env.___wasi_fd_seek": func(ctx exec.Context, x, y, z, w uint32) uint32 {
+		return 8
+	},
+	"wasi_fd_write": func(ctx exec.Context, x, y, z, w uint32) uint32 {
+		return 8
+	},
+	"env.___wasi_environ_sizes_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 0
+	},
+	"env.___wasi_environ_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 0
+	},
+	"env.___wasi_args_sizes_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 0
+	},
+	"env.___wasi_args_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 0
+	},
+	"env.___wasi_proc_exit": func(ctx exec.Context, x uint32) uint32 {
+		exec.Throw(exec.NewTrap("exit"))
+		return 0
+	},
 })
 
 // NewResolver return exec.Resolver which resolves symbols needed by wasi environment

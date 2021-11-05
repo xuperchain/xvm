6cae2721575dd89db01a73d150f210b4b887f2c5
chenfengjin <1871653365@qq.com>
2021-11-05 11:50:32 +0800

    Add README.md

---
+++ example/example_add.go
@@ -0,0 +1,67 @@
+package example
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/xuperchain/xvm/compile"
+	"github.com/xuperchain/xvm/exec"
+	"github.com/xuperchain/xvm/runtime/emscripten"
+)
+
+func compileLibrary(wasmpath string) (string, error) {
+	tmpdir, err := ioutil.TempDir("", "xvm-exec-test")
+	if err != nil {
+		return "", err
+	}
+	defer os.RemoveAll(tmpdir)
+	cfg := &compile.Config{
+		Wasm2cPath: "wasm2c",
+		OptLevel:   0,
+	}
+	libpath := replaceExt(wasmpath, ".so")
+	err = compile.CompileNativeLibrary(cfg, libpath, wasmpath)
+	if err != nil {
+		return "", err
+	}
+	return libpath, nil
+}
+
+func replaceExt(name, ext string) string {
+	dir, file := filepath.Split(name)
+	idx := strings.Index(file, ".")
+	if idx == -1 {
+		file = file + ext
+	} else {
+		file = file[:idx] + ext
+	}
+	return filepath.Join(dir, file)
+}
+
+func ExampleSpec() {
+	modulePath, err := compileLibrary("testdata/add.wasm")
+	if err != nil {
+		log.Fatal(err)
+	}
+	resolver := emscripten.NewResolver()
+	code, err := exec.NewAOTCode(modulePath, resolver)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, err := code.NewContext(&exec.ContextConfig{GasLimit: exec.MaxGasLimit})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ctx.Release()
+	ret, err := ctx.Exec("_add", []int64{1, 2})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ret)
+	// Output: 3
+}

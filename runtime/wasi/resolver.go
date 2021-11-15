6d608100597ff108180aa7541bfffb063a7981d0
chenfengjin <1871653365@qq.com>
2021-11-15 19:00:48 +0800

    Add more

--- runtime/wasi/resolver.go
+++ runtime/wasi/resolver.go
@@ -74,6 +74,41 @@ var resolver = exec.MapResolver(map[string]interface{}{
 		exec.Throw(exec.NewTrap("exit"))
 		return 0
 	},
+	// for 1.40
+	"wasi_snapshot_preview1.fd_prestat_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 8
+	},
+	"wasi_snapshot_preview1.fd_fdstat_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 8
+	},
+	"wasi_snapshot_preview1.fd_prestat_dir_name": func(ctx exec.Context, x, y, z uint32) uint32 {
+		return 8
+	},
+	"wasi_snapshot_preview1.fd_close": func(ctx exec.Context, x uint32) uint32 {
+		return 8
+	},
+	"wasi_snapshot_preview1.fd_seek": func(ctx exec.Context, x, y, z, w uint32) uint32 {
+		return 8
+	},
+	"wasi_snapshot_preview1.fd_write": func(ctx exec.Context, x, y, z, w uint32) uint32 {
+		return 8
+	},
+	"wasi_snapshot_preview1.environ_sizes_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 0
+	},
+	"wasi_snapshot_preview1.environ_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 0
+	},
+	"wasi_snapshot_preview1.args_sizes_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 0
+	},
+	"wasi_snapshot_preview1.args_get": func(ctx exec.Context, x, y uint32) uint32 {
+		return 0
+	},
+	"wasi_snapshot_preview1.proc_exit": func(ctx exec.Context, x uint32) uint32 {
+		exec.Throw(exec.NewTrap("exit"))
+		return 0
+	},
 })
 
 // NewResolver return exec.Resolver which resolves symbols needed by wasi environment

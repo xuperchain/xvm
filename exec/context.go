591c611c631d200e781c6b15488767ec1006736d
FengjinChen <19762931+chenfengjin@users.noreply.github.com>
2022-02-25 19:02:11 +0800

    exec: add llvm backend support (#10)

--- exec/context.go
+++ exec/context.go
@@ -22,6 +22,10 @@ type ErrFuncNotFound struct {
 func (e *ErrFuncNotFound) Error() string {
 	return fmt.Sprintf("%s not found", e.Name)
 }
+func (e *ErrFuncNotFound) Is(err error) bool {
+	err1, ok := err.(*ErrFuncNotFound)
+	return ok && err1.Name == e.Name
+}
 
 // ContextConfig configures an execution context
 type ContextConfig struct {

aca099f6738488f87b9acef85cb480b9106c218e
FengjinChen <19762931+chenfengjin@users.noreply.github.com>
2022-06-06 10:24:25 +0800

    examples: add assembly script example (#31)

---
+++ example/assembly-script/add/README.md
@@ -0,0 +1,8 @@
+### build 
+
+
+參考 [AssemblyScript 官方文檔](https://www.assemblyscript.org/getting-started.html#setting-up-a-new-project)
+
+
+
+
---
+++ example/assembly-script/add/example_add_test.go
@@ -0,0 +1,91 @@
+package rust
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/xuperchain/xvm/compile"
+	"github.com/xuperchain/xvm/debug"
+	"github.com/xuperchain/xvm/exec"
+	"github.com/xuperchain/xvm/runtime/emscripten"
+	"github.com/xuperchain/xvm/runtime/wasi"
+)
+
+func compileLibrary(wasmpath string) (string, error) {
+	tmpdir, err := ioutil.TempDir("", "xvm-exec-test")
+	if err != nil {
+		return "", err
+	}
+	defer os.RemoveAll(tmpdir)
+	cfg := &compile.Config{
+		Wasm2cPath: "wasm2c",
+		OptLevel:   0,
+	}
+	libpath := replaceExt(wasmpath, ".so")
+	err = compile.CompileNativeLibrary(cfg, libpath, wasmpath)
+	if err != nil {
+		return "", err
+	}
+	return libpath, nil
+}
+
+func replaceExt(name, ext string) string {
+	dir, file := filepath.Split(name)
+	idx := strings.Index(file, ".")
+	if idx == -1 {
+		file = file + ext
+	} else {
+		file = file[:idx] + ext
+	}
+	return filepath.Join(dir, file)
+}
+
+func ExampleAdd() {
+	modulePath, err := compileLibrary("testdata/build/release.wasm")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	code, err := exec.NewAOTCode(
+		filepath.Join(cwd, modulePath),
+		exec.NewMultiResolver(
+			emscripten.NewResolver(),
+			wasi.NewResolver(),
+		),
+		&exec.CodeConfig{
+			MemoryConfig: struct {
+				MemoryGrow exec.MemoryGrowConfig
+				Populate   exec.PopulateConfig
+				HugePage   exec.HugePageConfig
+			}{
+				MemoryGrow: exec.MemoryGrowConfig{
+					Enabled:    true,
+					Initialize: 10, Maximium: 65535,
+				},
+			},
+		})
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, err := code.NewContext(&exec.ContextConfig{GasLimit: exec.MaxGasLimit})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ctx.Release()
+	debug.SetWriter(ctx, os.Stdout)
+
+	ret, err := ctx.Exec("add", []int64{1, 2})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ret)
+	// Output: 3
+}
